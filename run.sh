b918b112f47d17a950318bba0ec50a7a25408ab2
Peter Volnov <neafiol.io@gmail.com>
2024-01-26 16:21:10 -0800

    feat: CORS

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Default command
-set -- uvicorn src.app:app --host 0.0.0.0 --port 8000 "$@"
+# Default command with required arguments
+CMD_ARGS=("uvicorn" "src.app:app" "--host" "0.0.0.0" "--port" "8000")
 
-# Check for --ssl argument
-if [[ " $* " == *" --ssl "* ]]; then
-    # Assume SSL key and cert are in /path/to/your/certs/
-    # Modify this path as necessary
-    set -- "$@" --ssl-keyfile=/ssl/certs/key.pem --ssl-certfile=/ssl/certs/cert.pem
-fi
+# Check for --ssl argument and append SSL configurations
+for arg in "$@"; do
+    if [[ $arg == "--ssl" ]]; then
+        # Assume SSL key and cert are in /ssl/certs/
+        # Modify this path as necessary
+        CMD_ARGS+=("--ssl-keyfile=/ssl/certs/key.pem" "--ssl-certfile=/ssl/certs/cert.pem")
+        break
+    fi
+done
 
-# Execute the command
-exec "$@"
+# Execute the command with additional arguments from command line
+exec "${CMD_ARGS[@]}" "$@"
